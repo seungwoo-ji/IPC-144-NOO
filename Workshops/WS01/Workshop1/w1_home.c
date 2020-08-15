a9f00829d7e014dd08a2b6fc70fb8b7fa39dcbd0
Seung Woo (Paul) Ji <wincow.ji@gmail.com>
2020-08-15 14:39:51 -0400

    Added Workshop 1

---
+++ Workshops/WS01/Workshop1/w1_home.c
@@ -0,0 +1,21 @@
+/*	Name:		 Seung Woo Ji
+	ID:			 swji1
+	Email:		 swji1@myseneca.ca
+	Section:	 NOO
+	Date:		 Sep. 11, 2019
+	Description: Workshop 1 - Home	*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("********************************\n");
+	printf("*** Welcome to C Programming ***\n");
+	printf("********************************\n");
+
+	char test[10] = { 'h', 'e', 'l', 'l','o'  };
+
+	printf("%s", test);
+
+	return 0;
+}
---
+++ Workshops/WS01/Workshop1/w1_lab.c
@@ -0,0 +1,15 @@
+/*	Name:		 Seung Woo Ji
+	ID:			 swji1
+	Email:		 swji1@myseneca.ca
+	Section:	 NOO
+	Date:		 Sep. 11, 2019
+	Description: Workshop 1 - Lab	*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("*** Welcome to C Programming ***\n");
+
+	return 0;
+}
